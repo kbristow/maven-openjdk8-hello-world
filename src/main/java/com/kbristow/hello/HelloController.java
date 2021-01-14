f21153108263afde7d1b5554cfa95495bb0b28dc
Kieran Bristow (ZA) <kieran.bristow@absa.africa>
2021-01-14 14:31:57 +0200

    Rewrite history

---
+++ src/main/java/com/kbristow/hello/HelloController.java
@@ -0,0 +1,13 @@
+package com.kbristow.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello, world from Java 8 :)";
+    }
+}
